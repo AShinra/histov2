d1cedf7660838cccb295c2e32e41c8054800c378
Athan <jonpuray@gmail.com>
2025-09-24 05:42:44 +0800

    updated

--- input.py
+++ input.py
@@ -63,6 +63,7 @@ def input(client, client_list):
         companies_list.append(company)
     
     agencies_list = set(agencies_list.sort())
+    st.write(agencies_list)
     agencies_list = {item for item in agencies_list if item not in [None, '']}
     companies_list = set(companies_list.sort())
 
