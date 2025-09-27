11d1b29818d8fb1e31831ec4095e6e1ec2a744af
Athan <jonpuray@gmail.com>
2025-09-28 01:52:30 +0800

    added user login

--- home.py
+++ home.py
@@ -1,10 +1,15 @@
 import streamlit as st
 from common import connect_to_mongodb
 from argon2 import PasswordHasher
-from main import main
+from main import main, get_logo
 
 
 if __name__ == '__main__':
+
+    try:
+        st.sidebar.image(get_logo())
+    except FileNotFoundError:
+        st.sidebar.write("Image file not found. Please check the path.")
 
     if "logged_in" not in st.session_state:
         st.session_state.logged_in = False
